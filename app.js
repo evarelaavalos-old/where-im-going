c2cf6b2efb0536baa9f67721df5a5ac60c9a2f57
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2021-11-03 20:30:18 -0300

    Adding a basic express logic to handle http requests

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+const express = require('express');
 const path = require('path');
 
 const scrapeGoogle = require('./src/services/scraper.js');
 
-(async () => {
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', express.static(path.join('src', 'public')));
+
+app.get('/api/links', async (req, res) => {
     let screenshotPath = path.join(__dirname, 'temp', 'screenshots');
     await scrapeGoogle(screenshotPath);
-})();
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`);
+})
